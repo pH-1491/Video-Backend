3d7d715dcc6c985db05f5b418fec932fd297a5b9
pH-1491 <priyeharsh19@gmail.com>
2025-08-09 01:08:13 +0530

    added register controller

--- src/app.js
+++ src/app.js
@@ -11,8 +11,24 @@ app.use(cors({
 
 )
 
-app.use(express.json({limit: "16kb}"}));
-app.use(express.urlencoded({ extended: true }));  //space -> %20 ya url me + use ho jata hi to wo sb ko handle krne ke liye hi
+app.use(express.json({limit: "25mb"}));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));  //space -> %20 ya url me + use ho jata hi to wo sb ko handle krne ke liye hi
 app.use(express.static("public")); //image ya favicon ye sb rkhne ke liye public directory
 app.use(cookieParser()); //serve se browser ki cookies control krne ke liye
+
+
+
+
+//routes import
+
+import userRouter from './routes/user.routes.js';
+
+//routes decleration
+
+app.use('/api/v1/users', userRouter);
+
+
+
+
+
 export { app };
