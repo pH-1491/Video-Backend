f88c4e5c0996acca3c7da56a9e123ee222491d8b
pH-1491 <priyeharsh19@gmail.com>
2025-08-24 21:43:38 +0530

    added some playlist controllers

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,7 +7,61 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+    const { channelId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(channelId)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    //count total subscribers
+    const totalSubscribers = await Subscription.countDocuments({channel: channelId});
+
+    // //total videos
+    // const totalVideos = await Video.countDocuments({owner: channelId});
+
+    //total views across all videos
+    const videoStats = await Video.aggregate([
+        { $match: {owner: new mongoose.Types.ObjectId(channelId) } },
+        {
+            $group: {
+                _id: null, // no filter is given to group them we are just collecting all videos with that channel id
+                totalViews: { $sum: "$views"},
+                totalVideos: {$sum: 1} //for every document that passes match add 1
+            }
+        }
+    ]);
+
+    const totalVideos = videoStats[0]?.totalVideos || 0;
+    const totalViews = videoStats[0]?.totalViews || 0;
+
+    //total likes
+    //to get this join with video schema
+    const likeStats = await Like.aggregate([
+        {
+            $lookup: {
+                from: 'videos',
+                localField: 'video',
+                foreignField: '_id',
+                as: 'videoData'
+
+            },
+
+        },
+        {
+            $unwind: '$videoData'
+        },
+        {
+            $match: {
+                "videoData.owner": new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $count: ""
+        }
+    ])
+
+
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,17 +3,38 @@ import {Playlist} from "../models/playlist.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {Video} from "../models/video.model.js";
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
-    //TODO: create playlist
+
+    if (!name){
+        throw new ApiError(400,'Playlist name is required')
+    }
+
+    const playList = await Playlist.create(
+        {
+            name: name,
+            description: description || " ",
+            owner: req.user._id,
+            videos: []
+        }
+    )
+
+    return res
+        .status(201)
+        .json(new ApiResponse(201,playList,"playlist created successfully."))
 })
+
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
@@ -23,12 +44,72 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+
+    //check if playlist exists
+    const playlist = await Playlist.findById(playlistId);
+    if (!playlist) {
+        throw new ApiError(400, 'Playlist not found')
+    }
+
+    //check if video to be added exists
+    const video = await Video.findById(videoId);
+    if (!video) {
+        throw new ApiError(400, 'Video not found')
+    }
+
+    //check if the video already exists in the playlist
+    if (playlist.videos.includes(videoId)) {
+        throw new ApiError(401, 'Video already exists in the playlist')
+    }
+
+    //add the video to the playlist
+    playlist.videos.push(videoId);
+    await playlist.save();
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(201,playlist,"video saved successfully to the playlist.")
+        )
+
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
     // TODO: remove video from playlist
+    const playlist = await Playlist.findById(playlistId);
+    if (!playlist) {
+        throw new ApiError(400, 'Playlist not found')
+    }
 
+    const video = await Video.findById(videoId);
+    if (!video) {
+        throw new ApiError(400, 'Video not found')
+    }
+
+    // playlist.video.findByIdAndDelete(videoId);
+    // playlist.save();
+    //this is wrong because playlist.video is an array object and not a model
+
+    playlist.videos = playlist.videos.filter(
+        (vid) => vid.toString() !== videoId
+    );
+
+    //or we can also use $pull
+    //await.Playlist.findByIdAndUpdate(
+    //playlistId,
+    //{ $pull: {videos: videoId} },
+    //{new: true}
+    //)
+
+    playlist.save();
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(201,playlist,"video deleted successfully from the playlist.")
+    )
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
