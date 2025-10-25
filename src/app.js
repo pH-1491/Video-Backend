5e0512dd63f4f92f8cd94acd029f1ee8d3699942
pH-1491 <priyeharsh19@gmail.com>
2025-10-25 21:42:53 +0530

    removed a small bug

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use('/api/v1/videos', videoRouter);
 
 
 app.get("/test", (req, res) => {
-    res.json({ message: "Test route working ✅" });
+    res.json({ message: "Test route working " });
 });
 
 export { app };
